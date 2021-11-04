6e1e39dc0bad92268395fac3da4e19dfe4004737
pavolhodas <pavolhodas4@gmail.com>
2021-11-04 08:56:40 +0100

    4th adjust programmer's salary

--- src/Programmer.java
+++ src/Programmer.java
@@ -3,4 +3,9 @@ public class Programmer extends Employee {
     public Programmer(float salary, int bonus) {
         super(EmployeeType.PROGRAMMER, salary, bonus);
     }
+    private final float programmerSalary = salary + bonus;
+
+    public void getInfo() {
+        System.out.println(employeeType + "'s salary is " + programmerSalary);
+    }
 }
