0a78f29bb3a3c14d8e50e421dd1eec01f16bd1f2
pavolhodas <pavolhodas4@gmail.com>
2021-11-04 08:49:08 +0100

    3th add lambda to main class

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<Employee> employe = new ArrayList<>();
+        employe.add(new Programmer(1200, 50));
+        employe.add(new Teacher(800, 100));
+        employe.add(new Driver(200000, 300000));
 
-        Programmer programmer = new Programmer(1200, 50);
-        Teacher teacher = new Teacher(800, 100);
-        Driver driver = new Driver(200000, 300000);
+        employe.forEach(Employee::getInfo);
 
-        programmer.getInfo();
-        teacher.getInfo();
-        driver.getInfo();
     }
 }
